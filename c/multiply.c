03f0e8116d10ef2af8548d2c0c4e43106691e690
U-GTECH\anna.v <anna.v@ua1-2242-pc1.gtech.local>
2019-03-18 15:20:08 +0200

    sum files are added

---
+++ c/multiply.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int sum(int a, int b) {
+    c = a + b;
+    return c;
+}
+int main() {
+    printf("Sum is equal to\n", sum(7,8));
+}
---
+++ js/sum.js
@@ -0,0 +1,7 @@
+function sum(){
+    var a = +prompt('Please enter 1st natural number', 0);
+    var b = +prompt('Please enter 2nd natural number', 0);
+    var c = a + b;
+	alert (c);
+}
+sum();
