a23757ece954e6e5165226301f43f30c9c245b88
U-GTECH\anna.v <anna.v@ua1-2242-pc1.gtech.local>
2019-03-21 20:39:10 +0200

    poem added

---
+++ js/poem.js
@@ -0,0 +1,8 @@
+function poem(counter) {
+    counter--;
+    alert('у попа была собака\n он её любил\n она съела кусок мяса, он её убил\n в землю закопал и надпись написал,\n что');
+    if (counter != 0)
+        poem(counter);
+    document.write('' + counter);
+}
+poem()
