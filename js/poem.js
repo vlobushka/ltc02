b266dc8f9d1773e823a6c9b338c7b2171ed1004f
U-GTECH\anna.v <anna.v@ua1-2242-pc1.gtech.local>
2019-03-21 20:41:37 +0200

    remowed trash from code

--- js/poem.js
+++ js/poem.js
@@ -2,7 +2,6 @@ function poem(counter) {
     counter--;
     alert('у попа была собака\n он её любил\n она съела кусок мяса, он её убил\n в землю закопал и надпись написал,\n что');
     if (counter != 0)
-        poem(counter);
-    document.write('' + counter);
+        poem(counter);    
 }
 poem()
