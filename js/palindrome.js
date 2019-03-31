01dffc17455a4a0ab09981c55775120401e515e6
vlobushka <vlobushka@gmmail.com>
2019-03-31 14:03:00 +0300

    palindrome4

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,9 +1,11 @@
 var input = prompt('Please enter text');
-var pal = input.split('');
+var low = input.toLowerCase();
+var pal = low.split('');
 var res = pal.reverse();
 var str = res.join('');
-if (str == input) {
+if (str == low) {
     console.log ('Palindrome');
 } else {
     console.log('Not a palindrome');
 }
+
