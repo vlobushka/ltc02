061beab7d741a43474325fa5b298db9f78e80bd6
vlobushka <vlobushka@gmmail.com>
2019-03-31 12:39:29 +0300

    bottles added

---
+++ js/bottles.js
@@ -0,0 +1,5 @@
+function bottles() {
+    console.log('99 bottles of beer on the wall, 99 bottles of beer.\n+ Take one down, pass it around, 98 bottles of beer on the wall...\n+ 98 bottles of beer on the wall, 98 bottles of beer.\n Take one down, pass it around, 97 bottles of beer on the wall...\n+97 bottles of beer on the wall, 97 bottles of beer.\n+Take one down, pass it around, 96 bottles of beer on the wall...\n');
+
+}
+bottles()
