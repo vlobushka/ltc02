0000fbf0082fe6767bdffcdae33876d15ba46618
vlobushka <vlobushka@gmmail.com>
2019-03-31 12:42:16 +0300

    bottles added2

--- js/bottles.js
+++ js/bottles.js
@@ -1,5 +1,12 @@
-function bottles() {
-    console.log('99 bottles of beer on the wall, 99 bottles of beer.\n+ Take one down, pass it around, 98 bottles of beer on the wall...\n+ 98 bottles of beer on the wall, 98 bottles of beer.\n Take one down, pass it around, 97 bottles of beer on the wall...\n+97 bottles of beer on the wall, 97 bottles of beer.\n+Take one down, pass it around, 96 bottles of beer on the wall...\n');
-
+function bottles(counter) {
+    counter--;
+	nextBottle= counter - 1;
+    if (counter > 1) {
+console.log(counter + ' bottles of beer on the wall,' + counter + ' bottles of beer.\nTake one down, pass it around,' + nextBottle + 'bottles of beer on the wall...');
+bottles(counter)
+    }
+    else  {
+        console.log('1 bottle of beer on the wall, 1 bottle of beer.\nTake it down, pass it around, no bottles of beer on the wall.\n')
+    };    
 }
-bottles()
+bottles(99)
