30b9fcf2e4dc1ef3ff7669fbb49835a0f33361db
vlobushka <vlobushka@gmmail.com>
2019-03-31 16:40:43 +0300

    palindrome5

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,11 +1,15 @@
-var input = prompt('Please enter text');
-var low = input.toLowerCase();
-var pal = low.split('');
-var res = pal.reverse();
-var str = res.join('');
-if (str == low) {
-    console.log ('Palindrome');
-} else {
-    console.log('Not a palindrome');
-}
-
+function palindrome(text) {
+    text = prompt('Please insert text here');
+    endChar = text.length - 1;
+    var low = text.toLowerCase();
+        if (endChar < 1) {
+            console.log('Palindrome')
+        } else {
+            if (low [0] == low [endChar]) {
+                   return palindrome(low.substring(1, endChar))
+        }   else{
+            console.log('Not a palindrome')
+            }
+        }  
+    }
+  palindrome()
